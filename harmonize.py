07705c80bccf6886ed9e60f94c2e8c2a83321364
Charlie Hanacek <charlie@czhanacek.com>
2017-10-07 15:22:02 -0700

    Less clicking!

--- harmonize.py
+++ harmonize.py
@@ -5,6 +5,7 @@ from multiprocessing import Queue
 from threading import Thread
 import time
 
+
 CHUNK = 22050
 FORMAT = pyaudio.paInt16
 swidth = 4
@@ -13,9 +14,9 @@ RATE = 44100
 
 
 
-def output_thread(frequency_queue):
+def fifth(frequency_queue):
     '''
-    processes the frequency queue as it comes in
+    processes the frequency queue as it comes in (a fifth of the input frequency)
     '''
 
     # basically while the frequency queue isn't empty, which it should never be....
@@ -35,17 +36,24 @@ def output_thread(frequency_queue):
         
         freq = frequency_queue.get() # get the most recent function in the queue. Should only run 
                                 # the first time the function is run.
-        WAVEDATA = ''
         i = 0
-        while(frequency_queue.empty()):
-            time.sleep(1/44100)
-            WAVEDATA = chr(int(math.sin(i / ((44100 / freq) / math.pi)) * 127 + 128))
+        while(1):
+            fifth = freq * pow(1.05945454545454545455, 7) * pow(2, 0)
+            third = freq * pow(1.05945454545454545455, 4) * pow(2, 0)
+            WAVEDATA = chr(int(math.sin(i / ((44100 / fifth) / math.pi)) * 127 + 128))
             outputstream.write(WAVEDATA)
+            if(frequency_queue.empty() == False and int(math.sin(i / ((44100 / fifth) / math.pi)) * 127) == 0):
+                while(frequency_queue.empty() == False):
+                    freq = frequency_queue.get()
+                i = 0
+            WAVEDATA = chr(int(math.sin(i / ((44100 / third) / math.pi)) * 127 + 128))
+            outputstream.write(WAVEDATA)
+            
             i += 1
 
 
-def input_thread(frequency_queue):
-    
+def input_thread(output_queue):
+    how_fast = 5
     paudio = pyaudio.PyAudio()
     stream = paudio.open(format=FORMAT,
                 channels=CHANNELS,
@@ -54,41 +62,46 @@ def input_thread(frequency_queue):
                 frames_per_buffer=CHUNK)
     old_freq = 0
     while(1):
-        time.sleep(0.1)
-        data = stream.read(RATE/10)
-        indata = np.fromstring(data, dtype=np.int16)
+        time.sleep(1 / how_fast)
+        data = stream.read(RATE / how_fast)
+        indata = np.fromstring(data, dtype=np.int32)
         # Take the fft and square each value
         fft_data=abs(np.fft.rfft(indata))**2
         # find the maximum
         which = fft_data[1:].argmax() + 1
-        freqstring = "The freq is "
         thefreq = 0
         # use quadratic interpolation around the max
         if which != len(fft_data)-1:
             y_0,y_1,y_2 = np.log(fft_data[which-1:which+2:])
             x_1 = (y_2 - y_0) * .5 / (2 * y_1 - y_2 - y_0)
             # find the frequency and output it
-            thefreq = (which+x_1)*RATE/(RATE / 10)
-            freqstring += str(thefreq) + " Hz"
+            thefreq = (which+x_1)*RATE/(RATE / how_fast)
+            
         else:
-            thefreq = which*RATE/(RATE/10)
-            freqstring += str(thefreq) + " Hz"
-        print(freqstring)
-        if(thefreq > 100):
+            thefreq = which*RATE/(RATE / how_fast)
+
+        thefreq *= pow(2, 2)
+        if(thefreq > 500):
+            #while(thefreq < 700):
+            #    thefreq *= 2
+            
+            
             if(abs(thefreq - old_freq) > 50):
+                print(thefreq)
+                print("difference was " + str(abs(thefreq - old_freq)))
                 old_freq = thefreq
-                
                 print("Sent")
-                frequency_queue.put(thefreq * pow(1.05945454545454545455, 7))
+                output_queue.put(thefreq)
 
     
 
-que = Queue()
-que.put(600) # 600 is the frequency that we get to open with
-t_2 = Thread(target=output_thread, args=(que,))
-t_2.start()
+queue = Queue()
+queue.put(600) # 600 is the frequency that we get to open with
 
-t_1 = Thread(target=input_thread, args=(que,))
+output = Thread(target=fifth, args=(queue,))
+output.start()
+
+t_1 = Thread(target=input_thread, args=(queue,))
 time.sleep(1)
 t_1.start()
 
